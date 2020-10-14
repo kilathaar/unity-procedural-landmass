3ab980966fbbfe965bc54fd67b9429312c73327e
Jimmy Ljungberg <jimmy.ljungberg@gmail.com>
2020-10-14 23:42:05 +0200

    Återställt konfiguration för SampleScene

--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class Noise {
 	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
